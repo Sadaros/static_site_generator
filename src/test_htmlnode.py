4a0ce80589e7f5b693b6330b0992b741b2dbbd16
Sadaros <post@chrislearns.tech>
2024-08-04 20:36:08 +0200

    added some tests for leafnode

--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,6 +1,6 @@
 import unittest
 from project_types import HTMLTag, TextType
-from htmlnode import HTMLNode
+from htmlnode import HTMLNode, LeafNode
 
 
 class TestHTMLNode(unittest.TestCase):
@@ -35,6 +35,25 @@ class TestHTMLNode(unittest.TestCase):
     def test_to_html(self):
         node = HTMLNode()
         self.assertRaises(NotImplementedError, node.to_html)
+
+
+class TestLeafNode(unittest.TestCase):
+    def test_eq(self):
+        node = LeafNode("Test", "p")
+        node2 = LeafNode("Test", "p")
+        self.assertEqual(node, node2)
+
+    def test_repr(self):
+        node = LeafNode("test", "a", {"href": "google.com"})
+        test_case = "LeafNode(a, \"test\", [], {'href': 'google.com'})"
+        self.assertEqual(repr(node), test_case)
+
+    def test_props_to_html(self):
+        node = LeafNode("test", props={"href": "google.com", "target": "_blank"})
+        test_case = ' href="google.com" target="_blank"'
+        self.assertEqual(node.props_to_html(), test_case)
+
+    def 
 
 
 if __name__ == "__main__":
