a0576a9d3b0959b804dcc38650761eada588ed55
Sadaros <post@chrislearns.tech>
2024-08-07 07:09:07 +0200

    added tests for text_node_to_html_node

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -1,6 +1,7 @@
 import unittest
 
-from textnode import TextNode
+from textnode import TextNode, text_node_to_html_node
+from htmlnode import LeafNode
 
 
 class TestTextNode(unittest.TestCase):
@@ -18,6 +19,33 @@ class TestTextNode(unittest.TestCase):
         node = TextNode("Test", "text")
         node2 = TextNode("Test", "bold")
         self.assertNotEqual(node, node2)
+
+
+class TestTextNodeToHTMLNode(unittest.TestCase):
+    def test_valid_text_type(self):
+        text_node = TextNode("TestCase", "text")
+        html_node = text_node_to_html_node(text_node)
+        self.assertEqual(html_node, LeafNode("TestCase"))
+
+    def test_invalid_text_type(self):
+        with self.assertRaises(ValueError):
+            node = TextNode(" ", "invalid")  # type: ignore
+            text_node_to_html_node(node)
+
+    def test_valid_link(self):
+        text_node = TextNode("A link", "link", "google.com")
+        html_node = text_node_to_html_node(text_node)
+        self.assertEqual(html_node, LeafNode("A link", "a", {"href": "google.com"}))
+
+    def test_missing_url(self):
+        with self.assertRaises(ValueError):
+            node = TextNode("link text", "link", "")
+            text_node_to_html_node(node)
+
+    def test_valid_image(self):
+        text_node = TextNode("alt text", "image", "cool_image.png")
+        html_node = LeafNode("", "img", {"src": "cool_image.png", "alt": "alt text"})
+        self.assertEqual(html_node, text_node_to_html_node(text_node))
 
 
 if __name__ == "__main__":
